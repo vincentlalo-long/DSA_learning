091225705def5f5382eeb66e6f2e556cffbf0157
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-04-28 17:20:02 +0700

    Update caculate_x^n.c

--- đệ quy _recursion/caculate_x^n.c
+++ đệ quy _recursion/caculate_x^n.c
@@ -6,3 +6,12 @@ double fastPower1(double x, int n)
 } 
 // T(n)=2T(n/2)+1 
 // Áp dụng định lí thợ : O(n)
+
+// Cách 2 : nhanh hơn
+double fastPower2(doublex, intn){
+ if (n == 0) return 1;
+ double y = fastPower(x, n / 2); 
+ if (n % 2 == 0) return y*y;
+ return y*y*x;
+}
+ //T(n)=T(n/2)+1 -> O(logn) 
