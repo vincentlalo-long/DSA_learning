7e4b8dad2582be052d9644ad63525d77fcb6d193
Nguyễn Đăng Long <nickmoi3056@gmail.com>
2025-05-05 21:58:06 +0700

    Update lý thuyết.c

--- lk đơn nối vòng/lý thuyết.c
+++ lk đơn nối vòng/lý thuyết.c
@@ -19,3 +19,11 @@ void printList(Node* head) {
 • Bài toán vòng tròn Josephus
 • Chuyển danh sách liên kết đơn thường sang dạng nối vòng
 
+• Biểu diễn playlist 
+• Dùng danh sách liên kết đơn nối vòng thay vì danh sách liên kết đơn thông thường
+• Repeat playlist dễ hơn
+• Điều phối tiến trình trong HDH theo cơ chế Round-robin Scheduling
+• Thông tin mỗi tiến trình sẽ được lưu trong 1 node (ID tiến trình, trạng thái, thời gianthực hiện còn lại, v.v)
+• Mỗi tiến trình sẽ được cấp phát 1 chu kỳ time slice dùng CPU. Sau mỗi khoảngtime
+slice, hệ điều hành sẽ thực hiện việc chuyển đổi (context switch) từ tiến trình hiện tại sang tiến trình tiếp theo trong danh sách. 
+• Việc thêm/xóa tiến trình và xoay vòng dễ dàng hơn với danh sách liên kết đơn nối vòng
